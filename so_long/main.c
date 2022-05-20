d8ed60222868bfa43b9acb3ca7678d6a28035526
Maxence EUDIER <meudier@e3r12p17.clusters.42paris.fr>
2022-05-20 14:41:51 +0200

    backgroud ok

--- so_long/main.c
+++ so_long/main.c
@@ -1,37 +1,98 @@
 #include <mlx.h>
 #include <mlx_int.h>
+#include <X11/keysym.h>
+
+#define WIDTH 1280
+#define HEIGHT 896
+#define GREEN 0x00560000
 
 typedef struct	s_vars {
 	void	*mlx;
 	void	*win;
 	void	*win2;
 	void	*img;
+	void	*background;
 	int		img_width;
 	int		img_height;
-	char	*relative_path;
+	char	*img_path;
+	char	*background_path;
+	int		background_width;
+	int		background_height;
 	int		i;
 	int		j;
 }				t_vars;
 
-int	key_hook(int keycode, t_vars *vars)
+
+int	handle_no_event(void *data)
 {
+	(void)data;
+	/* This function is to close the prog*/
+	return (0);
+}
+
+int	handle_input(int keysym, t_vars *vars)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_image(vars->mlx, vars->img);
+		mlx_clear_window(vars->mlx, vars->win);
+		mlx_destroy_window(vars->mlx, vars->win);
+	}
+	return (0);
+}
+
+int	key_move(int keycode, t_vars *vars)
+{
+	int i;
+	int j;
+
 	if (keycode == 115)
 	{
-		vars->relative_path = "./img/sprites_pause1.xpm";
-		vars->j += 10;
+		vars->img_path = "./img/front_static1.xpm";
+		if (vars->j <= HEIGHT - 64)
+			vars->j += 64;
 	}
 	if (keycode == 100)
 	{
-		vars->relative_path = "./img/sprites_R1.xpm";
-		vars->i += 10;
+		vars->img_path = "./img/right_static.xpm";
+		if (vars->i <= WIDTH - 64)
+			vars->i += 64;
 	}
 	if (keycode == 97)
-		vars->i -= 10;
+	{
+		vars->img_path = "./img/left_static.xpm";
+		if (vars->i >= 64)
+			vars->i -= 64;
+	}
 	if (keycode == 119)
-		vars->j -= 10;
+	{
+		vars->img_path = "./img/back_static.xpm";
+		if (vars->j >= 64)
+			vars->j -= 64;
+	}
+	if (keycode == XK_Escape)
+	{
+		mlx_destroy_image(vars->mlx, vars->img);
+		mlx_destroy_image(vars->mlx, vars->background);
+		mlx_clear_window(vars->mlx, vars->win);
+		mlx_destroy_window(vars->mlx, vars->win);
+		return (0);
+	}
+
 	mlx_destroy_image(vars->mlx, vars->img);
 	mlx_clear_window(vars->mlx, vars->win);
-	vars->img = mlx_xpm_file_to_image(vars->mlx, vars->relative_path, &vars->img_width, &vars->img_height);
+	i = 0;
+	while (vars->background_width + i <= WIDTH)
+	{
+		j = 0;
+		while (vars->background_height + j <= HEIGHT)
+		{
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->background, 0 + i, 0 + j);
+			j += vars->background_height;
+		}
+		i += vars->background_width;
+	}
+	vars->img = mlx_xpm_file_to_image(vars->mlx, vars->img_path, &vars->img_width, &vars->img_height);
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0 + vars->i, 0 + vars-> j);
 	return (0);
 }
@@ -39,17 +100,46 @@ int	key_hook(int keycode, t_vars *vars)
 int	main(void)
 {
 	t_vars	vars;
+	int		i;
+	int		j;
 
-	vars.relative_path = "./img/sprites_pause1.xpm";
+	vars.background_path = "./img/herbe.xpm";
+	vars.img_path = "./img/front_static1.xpm";
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1280, 800, "Hello world!");
-	vars.img = mlx_xpm_file_to_image(vars.mlx, vars.relative_path, &vars.img_width, &vars.img_height);
-	vars.i = 10;
-	vars.j = 10;
-
+	if (!vars.mlx)
+		return (1);
+	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "Hello world!");
+	if (!vars.win)
+	{
+		free (vars.win);
+		return (1);
+	}
+	vars.i = 5;
+	vars.j = 5;
+	vars.background = mlx_xpm_file_to_image(vars.mlx, vars.background_path, &vars.background_width, &vars.background_height);
+	vars.img = mlx_xpm_file_to_image(vars.mlx, vars.img_path, &vars.img_width, &vars.img_height);
+	
+	
+	i = 0;
+	while (vars.background_width + i <= WIDTH)
+	{
+		j = 0;
+		while (vars.background_height + j <= HEIGHT)
+		{
+			mlx_put_image_to_window(vars.mlx, vars.win, vars.background, 0 + i, 0 + j);
+			j += vars.background_height;
+		}
+		i += vars.background_width;
+	}
+	
 	mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 10, 10);
+	
+	
+	mlx_key_hook(vars.win, key_move, &vars);
 
-	mlx_key_hook(vars.win, key_hook, &vars);
 
+	mlx_loop_hook(vars.mlx, &handle_no_event, &vars);
 	mlx_loop(vars.mlx);
+	mlx_destroy_display(vars.mlx);
+	free (vars.mlx);
 }
